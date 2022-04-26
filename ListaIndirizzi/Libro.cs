a91aad53127b4a25904fed10ff0c4efffab4e170
Gennaro <gennarinodi02@gmail.com>
2022-04-26 13:07:38 +0200

    commit

---
+++ ListaIndirizzi/Libro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzo
+    {
+        private string nome;
+        private string cognome;
+        private string via;
+        private string citta;
+        private string provincia;
+        private int codice_postale;
+
+
+        public Indirizzo(string nome, string cognome, string via, string citta, string provincia, int codice_postale)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.via = via;
+            this.citta = citta;
+            this.provincia = provincia;
+            this.codice_postale = codice_postale;
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            string riga = "------INDIRIZZO---------\n";
+            riga += "Nome:" + this.nome + "\n";
+            riga += "Cognome:" + this.cognome + "\n";
+            riga += "Via:" + this.via + "\n";
+            riga += "Città:" + this.citta + "\n";
+            riga += "Provincia:" + this.provincia + "\n";
+            riga += "Codice Postale:" + this.codice_postale + "\n";
+            return riga;
+
+
+        }
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaIndirizzi;
+string Path = "C:/Users/genna/OneDrive/Desktop/C#/ListaIndirizzi/addresses.csv";
+List<Indirizzo> listaDiIndirizzi = new List<Indirizzo>();
+StreamReader file = File.OpenText(Path);
+string rigaDaButtare = file.ReadLine();
+while (!file.EndOfStream)
+{
+    string riga=file.ReadLine();
+    string[] Info=riga.Split(',');
+    
+
+    if(Info.Length==6)
+    {
+        string nome=Info[0];
+        string cognome=Info[1];
+        string via=Info[2];
+        string citta=Info[3];
+        string provincia=Info[4];
+        int CodicePostale=int.Parse(Info[5]);
+
+        Indirizzo Indirizzi = new Indirizzo(nome, cognome, via, citta, provincia, CodicePostale);
+        listaDiIndirizzi.Add(Indirizzi);
+        Console.WriteLine(riga);
+    }
+
+}
+file.Close();
+foreach (Indirizzo indirizzo in listaDiIndirizzi)
+{
+   Console.WriteLine(indirizzo.ToString());
+}
